d2e36a5bb78c4a9cff35feab1cb47ce56bbf0afe
Pavel Rodionov <pavel@garin-studio.ru>
2014-07-17 15:28:32 +0400

    fix install generator

--- lib/generators/sape/install_generator.rb
+++ lib/generators/sape/install_generator.rb
@@ -2,11 +2,11 @@ require 'rails/generators'
 
 module Sape
   class InstallGenerator < ::Rails::Generators::Base
-    source_root File.expand_path("../../../", __FILE__)
+    source_root File.expand_path("../../app/assets/stylesheets", __FILE__)
     desc "This generator installs Sape to Asset Pipeline"
 
     def add_assets
-      template('app/assets/stylesheets/sape.css', 'app/assets/stylesheets/sape.css')
+      copy_file 'sape.css', 'app/assets/stylesheets/sape.css'
     end
 
   end
